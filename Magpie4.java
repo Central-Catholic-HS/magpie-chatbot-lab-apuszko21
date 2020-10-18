9851d2eed17185832a8ebb13abc8d17f10e71128
apuszko21 <apuszko21@centralcatholichs.com>
2020-10-18 19:44:14 -0400

    part 4 done

--- Magpie4.java
+++ Magpie4.java
@@ -53,8 +53,14 @@ public class Magpie4
 		{
 			response = transformIWantToStatement(statement);
 		}
-		else if (findKeyword(statement, "you", 0) >= 0){
+		else if (findKeyword(statement, "you", 0) >= 0 && statement.indexOf("You") == 0){
 			response = transformYouMeStatement(statement);
+		}
+		else if (findKeyword(statement, "I want", 0) >= 0){
+			response = transformIWantStatement(statement);
+		}
+		else if (findKeyword(statement, "I", 0) >= 0 && findKeyword(statement, "you", 0) >= 0 && statement.indexOf("I") == 0){
+			response = transformIYouStatement(statement);
 		}
 		else
 			response = getRandomResponse();
@@ -67,6 +73,22 @@ public class Magpie4
 	 * @param statement the user statement, assumed to contain "I want to"
 	 * @return the transformed statement
 	 */
+	private String transformIWantStatement(String statement){
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you be really happy if you had " + restOfStatement + "?";
+
+	}
+
 	private String transformIWantToStatement(String statement)
 	{
 		//  Remove the final period, if there is one
@@ -110,7 +132,21 @@ public class Magpie4
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
 	
-	
+	private String transformIYouStatement(String statement){
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() -1);
+		if (lastChar.equals(".")){
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+
+		int posI = findKeyword(statement, "I", 0);
+		int posYou = findKeyword(statement, "you", 0);
+
+		String restOfStatement = statement.substring(posI + 1, posYou);
+		return "Why do you" + restOfStatement + "me?";
+
+	}
 
 	
 	
